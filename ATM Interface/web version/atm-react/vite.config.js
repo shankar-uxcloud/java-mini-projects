cfb4525ff7db5b9204852b4a0bd7de79b4bc732e
P SHANKAR <shankarsourav400@gmail.com>
2026-09-23 00:32:37 +0530

    Fix base path case sensitivity in vite.config.js

--- ATM Interface/web version/atm-react/vite.config.js
+++ ATM Interface/web version/atm-react/vite.config.js
@@ -2,6 +2,6 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/Java-mini-projects/atm/',
+  base: '/java-mini-projects/atm/',
   plugins: [react()],
 })
